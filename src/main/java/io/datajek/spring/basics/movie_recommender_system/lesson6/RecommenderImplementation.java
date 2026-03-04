3fb0aefdc81cc61bd6a338eaa6799d3d0bbc41df
madhuedipelli <madhuedipelli@outlook.com>
2026-03-04 22:47:14 +0530

    intro to @Qualifier

--- src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson6/RecommenderImplementation.java
+++ src/main/java/io/datajek/spring/basics/movie_recommender_system/lesson6/RecommenderImplementation.java
@@ -1,15 +1,17 @@
 package io.datajek.spring.basics.movie_recommender_system.lesson6;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RecommenderImplementation {
 
     @Autowired
-    private Filter contentBasedFilter;
+    @Qualifier("CF")
+    private Filter filter;
     public String[] recommendMovies(String movie){
-        System.out.println("\nName of the filter in use: " + contentBasedFilter + "\n");
-        return contentBasedFilter.getRecommendations(movie);
+        System.out.println("\nName of the filter in use: " + filter + "\n");
+        return filter.getRecommendations(movie);
     }
 }
